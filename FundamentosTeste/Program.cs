58f24291f776ff8be971e3f19fa6cef690dcb334
Paulo <paulodiasred@gmail.com>
2024-07-20 16:38:38 -0300

    Estruturas de repetição FOR, While e Do While

--- FundamentosTeste/Program.cs
+++ FundamentosTeste/Program.cs
@@ -1,17 +1,72 @@
 ﻿using System.Data.Common;
 using FundamentosTeste.Models;
 
-Calculadora calc = new Calculadora();
+int soma = 0, numero = 0;
 
-calc.Somar(10, 30);
-calc.Subtrair(10, 50);
-calc.Multiplicar(15, 45);
-calc.Dividir(2, 2);
-calc.Potencia(3, 3);
-calc.Seno(30);
-calc.Coseno(30);
-calc.Tangente(30);
-calc.RaizQuadrada(9);
+
+do
+{
+    Console.WriteLine("Digite um numero (0 para parar)");
+    numero = int.Parse(Console.ReadLine());
+
+    soma += numero;
+
+} while(numero != 0);
+
+Console.WriteLine($"Total da soma dos numeros digitados e: {soma}");
+
+
+
+
+
+
+
+
+
+
+
+
+// int numero = 5;
+// int contador = 0;
+
+// while (contador <= 10)
+// {
+//     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+//     contador++;
+// }
+
+
+
+
+
+
+
+// estruturas de repetição em C#
+// FOR
+// int numero = 5;
+
+// for (int contador = 0; contador <= 10; contador++)
+// {
+//     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+// }
+
+
+
+
+
+
+
+// Calculadora calc = new Calculadora();
+
+// calc.Somar(10, 30);
+// calc.Subtrair(10, 50);
+// calc.Multiplicar(15, 45);
+// calc.Dividir(2, 2);
+// calc.Potencia(3, 3);
+// calc.Seno(30);
+// calc.Coseno(30);
+// calc.Tangente(30);
+// calc.RaizQuadrada(9);
 
 
 
