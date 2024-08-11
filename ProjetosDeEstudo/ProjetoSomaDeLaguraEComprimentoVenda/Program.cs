6f048c558a2a7bae8204c5de52d9be7e0f6b78f7
Paulo Dias <paulodiasred@gmail.com>
2024-08-11 19:44:14 -0300

    Add files via upload

---
+++ ProjetosDeEstudo/ProjetoSomaDeLaguraEComprimentoVenda/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+    class Program {
+        static void Main(string[] args) {
+
+            double largura, comprimento, precoMetroQuadrado, area, preco;
+        Console.WriteLine("Digite a largura");
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine("Digite o comprimento");
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine("Digite o preço por metro quadrado"); 
+            precoMetroQuadrado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = largura * comprimento;
+            preco = area * precoMetroQuadrado;
+
+            Console.WriteLine("AREA = " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PRECO = " + preco.ToString("F2", CultureInfo.InvariantCulture));
+            
+            Console.ReadLine();
+        }
+    }
