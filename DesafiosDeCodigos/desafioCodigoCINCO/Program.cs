905622ea49c9e1990b40c0d88d6fd19f30e8a45a
Paulo Dias <paulodiasred@gmail.com>
2024-08-11 19:50:16 -0300

    Add files via upload

---
+++ DesafiosDeCodigos/desafioCodigoCINCO/Program.cs
@@ -0,0 +1,54 @@
+﻿using System; 
+
+class minhaClasse { 
+
+  static void Main(string[] args) {  
+
+    double salario = 0.00; 
+    double reajuste = 0.00; 
+    double novoSalario = 0.00; 
+    double percentual = 0.00; 
+  
+    salario = Convert.ToDouble(Console.ReadLine()); 
+
+//TODO: Complete os espaços em branco com uma possível solução para o problema:
+   
+    
+
+    if(salario < 0) { 
+      return; 
+
+    } else if (salario <= 400.00) {
+      percentual = 15;
+      reajuste = salario * 0.15; 
+      novoSalario = salario + reajuste; 
+
+
+    } else if (salario <= 800.00) { 
+      percentual = 12; 
+      reajuste = salario * 0.12;         
+      novoSalario = salario + reajuste; 
+
+    } else if (salario <= 1200.00) { 
+      percentual = 10; 
+      reajuste = salario * 0.10; 
+      novoSalario = salario + reajuste; 
+
+    } else if (salario <= 2000.00) { 
+      percentual = 7; 
+      reajuste = salario * 0.07; 
+      novoSalario = salario + reajuste; 
+
+    } else { 
+      percentual = 4; 
+      reajuste = salario * 0.04; 
+      novoSalario = salario + reajuste; 
+
+    } 
+
+    Console.WriteLine("Novo salario: {0:0.00}", novoSalario); 
+    Console.WriteLine("Reajuste ganho: {0:0.00}",reajuste); 
+    Console.WriteLine("Em percentual: {0} %", percentual); 
+
+  } 
+}
---
+++ DesafiosDeCodigos/desafioCodigoQUATRO/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        
+        int[] num = new int[n];
+    
+// TODO: Crie as outras condições necessárias para a resolução do desafio:
+{
+    
+}
+        for (int count = 0; count < n; count++)
+        {
+            num[count] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine(MajorityElement(num));
+        
+    }
+    public static int MajorityElement(int[] nums)
+    {
+        int major = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.Length; i++ )
+        {
+            if (count == 0)
+            {
+                major = nums[i];
+                count = 1; //
+            }
+            else
+            {
+                if (major == nums[i])
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                }
+            }
+        }
+        return major;
+    }
+}
+
