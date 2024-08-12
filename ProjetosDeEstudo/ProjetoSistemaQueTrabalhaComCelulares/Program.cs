d9b52623743b9be13d2b18c7bc3768f7f7fb26bd
Paulo <paulodiasred@gmail.com>
2024-08-12 12:40:17 -0300

    Adição projeto trabalhando com celulares

---
+++ ProjetosDeEstudo/ProjetoSistemaQueTrabalhaComCelulares/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "31984652332", modelo: "Modelo 1", imei: "777777777", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone(numero: "31976542233", modelo: "Modelo 2", imei: "333333333", memoria: 64);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
