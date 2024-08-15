a6845efd8d8da4fd5e309432574aae4af8351801
Paulo <paulodiasred@gmail.com>
2024-08-14 22:01:54 -0300

    Entity Framework

--- ModuloAPI/Controllers/UsuarioController.cs
+++ ModuloAPI/Controllers/UsuarioController.cs
@@ -6,18 +6,29 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ModuloAPI.Controllers
 {
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class UsuarioController : ControllerBase
     {
-        [ApiController]
-        [Route("[controller]")]
-        public class UsuarioController : ControllerBase
+        [HttpGet("ObterDataHoraAtual")]
+        public IActionResult ObterDataHora()
         {
-            public IActionResult ObterDataHora()
+            var obj = new
             {
-                var obj = new
-                {
-                    Data = DateTime
-                }
-            }
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToShortTimeString()
+            };
+
+            return Ok(obj);
         }
+
+        [HttpGet("Apresentar/{nome}")]
+        public IActionResult Apresentar(string nome)
+        {
+            var mensagem = $"Olá {nome}, seja bem vindo!";
+            return Ok(new { mensagem });
+        }
+
     }
 }
