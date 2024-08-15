efc7dd00a0ddac2d665d5c5decfa155c74fea2bc
Paulo <paulodiasred@gmail.com>
2024-08-15 17:29:05 -0300

    Implementação Controller para adição de contatos na tabela

--- ModuloAPI/obj/Debug/net8.0/ModuloAPI.AssemblyInfo.cs
+++ ModuloAPI/obj/Debug/net8.0/ModuloAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5eca7583cbe538e5db4c4f164c16807477ad820")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d292af05c1fdddcdefaba899d42ec34ef3bda67")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
