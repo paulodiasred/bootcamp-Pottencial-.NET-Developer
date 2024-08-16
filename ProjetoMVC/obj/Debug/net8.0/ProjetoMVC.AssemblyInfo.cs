7f7f2e18857e546ca0a868a97012c83b56a0a306
Paulo <paulodiasred@gmail.com>
2024-08-16 18:06:01 -0300

    fix --erro de digitação

--- ProjetoMVC/obj/Debug/net8.0/ProjetoMVC.AssemblyInfo.cs
+++ ProjetoMVC/obj/Debug/net8.0/ProjetoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43c090eb5e70d6edddf6acbe7c370c3f996a3318")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+239caaf14392e83a8aa01a722ceeeb8ea1a284c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
